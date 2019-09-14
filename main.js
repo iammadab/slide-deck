1abf83a4679a313f669862cd79b436db405890b4
iammadab <iammadab@gmail.com>
2019-09-14 18:18:57 +0100

    Sending the images to the slides file

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const path = require("path")
+const fs = require("fs")
+const { promisify } = require("util")
 
 const app = express()
 const server = require("http").createServer(app)
@@ -23,8 +25,9 @@ app.get("/", (req, res) => {
 	res.render("slider-info")
 })
 
-app.get("/slides", (req, res) => {
-	res.render("slider")
+app.get("/slides", async (req, res) => {
+	let images = await getImages()
+	res.render("slider", { images })
 })
 
 app.get("/slides/controller", (req, res) => {
@@ -38,3 +41,19 @@ app.get("/slides/controller", (req, res) => {
 server.listen(PORT, () => {
 	console.log("Application listening at port " + PORT)
 })
+
+
+
+
+
+
+// Helper functions
+async function getImages(){
+	let readdir = promisify(fs.readdir)
+	let imageNames = await readdir(path.resolve(__dirname, "public", "img"))
+	return imageNames.map(renameImage)
+}
+
+function renameImage(current){
+	return `/img/${current}`
+}
