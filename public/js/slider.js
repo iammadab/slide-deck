5721c03946ed0f0a839d76434749c378d1c05b52
iammadab <iammadab@gmail.com>
2019-09-14 16:59:18 +0100

    Created a function to activate a slide

--- public/js/slider.js
+++ public/js/slider.js
@@ -20,6 +20,15 @@ function getLargestSlideId(){
 
 function getNextId(id, largest){
 	let next =  (id + 1) % largest
+	// When the next number is the number just before the last, the above returns 0 
+	// What I want is the largest
+	// So the tenary operator below is saying if the number is 0 return the largest else return the number
 	return next ? next : largest
 }
 
+function activateSlide(id){
+	let slideToActivate = document.querySelector(`[data-id="${id}"]`)
+	store.currentSlide.classList.remove("active")
+	slideToActivate.classList.add("active")
+	store.currentSlide = slideToActivate
+}
