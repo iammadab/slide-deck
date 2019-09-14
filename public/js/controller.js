d5310f95831bea773ea4ee2e03af5122f28e1a2b
iammadab <iammadab@gmail.com>
2019-09-14 18:05:41 +0100

    Listening for swipe events and sending io commands

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,6 +1,28 @@
-;(function attachEvents(){
-	// window.addEventListener("dbclic")
+let store = {
+	controlPad: document.querySelector(".slider-controller-section"),
+	socket: null
+}
+
+;(function getSocket(){
+	store.socket = io.connect()
 })()
+
+;(function attachHammerEvents(){
+	let hammertime = new Hammer(store.controlPad)
+	hammertime.on("doubletap", toggleFullScreen)
+	hammertime.on("swipeleft", executeLeft)
+	hammertime.on("swiperight", executeRight)
+})()
+
+function executeRight(){
+	if(store.socket)
+		store.socket.emit("action", { action: "prev" })
+}
+
+function executeLeft(){
+	if(store.socket)
+		store.socket.emit("action", { action: "next" })
+}
 
 function toggleFullScreen(){
 	if(!document.fullscreenElement)
